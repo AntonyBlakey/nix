1fff1464926e365a606271e417e36a9826e85cc5
Antony Blakey <antony.blakey@gmail.com>
2024-08-12 17:17:55 +1000

    improve homebrew config

--- modules/darwin/homebrew.nix
+++ modules/darwin/homebrew.nix
@@ -2,23 +2,54 @@
 {
 
   imports = [ inputs.nix-homebrew.darwinModules.nix-homebrew ];
+  
+    # https://github.com/zhaofengli/nix-homebrew
+    nix-homebrew = {
+      # Install Homebrew under the default prefix
+      enable = true;
 
-  # https://github.com/zhaofengli/nix-homebrew
-  nix-homebrew = {
-    # Install Homebrew under the default prefix
-    enable = true;
+      # Apple Silicon Only: Also install Homebrew under the default Intel prefix for Rosetta 2
+      enableRosetta = false;
 
-    # Apple Silicon Only: Also install Homebrew under the default Intel prefix for Rosetta 2
-    enableRosetta = false;
+      # User owning the Homebrew prefix
+      user = "antony";
+      group = "staff";
 
-    # User owning the Homebrew prefix
-    user = "antony";
-    group = "staff";
+      mutableTaps = true;
+    };
 
-    mutableTaps = true;
-  };
+    homebrew = {
+      enable = true;
 
-  homebrew.enable = true;
-  homebrew.casks = [ "nikitabobko/tap/aerospace" ];
+      onActivation = {
+        autoUpdate = false;
+        # 'zap': uninstalls all formulae(and related files) not listed here.
+        cleanup = "zap";
+      };
+
+      # Applications to install from Mac App Store using mas.
+      # You need to install all these Apps manually first so that your apple account have records for them.
+      # otherwise Apple Store will refuse to install them.
+      # For details, see https://github.com/mas-cli/mas 
+      masApps = {
+        # Xcode = 497799835;
+      };
+
+      taps = [
+        "nikitabobko/tap"
+        "FelixKratz/formulae"
+      ];
+
+      # `brew install`
+      brews = [
+        # "borders"
+      ];
+
+      # `brew install --cask`
+      casks = [
+        "aerospace"
+        # "visual-studio-code"
+      ];
+    };
 
 }
