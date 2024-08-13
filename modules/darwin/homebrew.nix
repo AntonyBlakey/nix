cce50290ccf0d6e78b7aa4e4ff22ef4b0a9c2f8b
Antony Blakey <antony.blakey@gmail.com>
2024-08-13 18:02:48 +1000

    Remove aerospace

--- modules/darwin/homebrew.nix
+++ modules/darwin/homebrew.nix
@@ -2,54 +2,44 @@
 {
 
   imports = [ inputs.nix-homebrew.darwinModules.nix-homebrew ];
-  
-    # https://github.com/zhaofengli/nix-homebrew
-    nix-homebrew = {
-      # Install Homebrew under the default prefix
-      enable = true;
 
-      # Apple Silicon Only: Also install Homebrew under the default Intel prefix for Rosetta 2
-      enableRosetta = false;
+  # https://github.com/zhaofengli/nix-homebrew
+  nix-homebrew = {
+    # Install Homebrew under the default prefix
+    enable = true;
 
-      # User owning the Homebrew prefix
-      user = "antony";
-      group = "staff";
+    # Apple Silicon Only: Also install Homebrew under the default Intel prefix for Rosetta 2
+    enableRosetta = false;
 
-      mutableTaps = true;
+    # User owning the Homebrew prefix
+    user = "antony";
+    group = "staff";
+
+    mutableTaps = true;
+  };
+
+  homebrew = {
+    enable = true;
+
+    onActivation = {
+      autoUpdate = false;
+      # 'zap': uninstalls all formulae(and related files) not listed here.
+      cleanup = "zap";
     };
 
-    homebrew = {
-      enable = true;
+    # Applications to install from Mac App Store using mas.
+    # You need to install all these Apps manually first so that your apple account have records for them.
+    # otherwise Apple Store will refuse to install them.
+    # For details, see https://github.com/mas-cli/mas 
+    masApps = { };
 
-      onActivation = {
-        autoUpdate = false;
-        # 'zap': uninstalls all formulae(and related files) not listed here.
-        cleanup = "zap";
-      };
+    taps = [ ];
 
-      # Applications to install from Mac App Store using mas.
-      # You need to install all these Apps manually first so that your apple account have records for them.
-      # otherwise Apple Store will refuse to install them.
-      # For details, see https://github.com/mas-cli/mas 
-      masApps = {
-        # Xcode = 497799835;
-      };
+    # `brew install`
+    brews = [ ];
 
-      taps = [
-        "nikitabobko/tap"
-        "FelixKratz/formulae"
-      ];
-
-      # `brew install`
-      brews = [
-        # "borders"
-      ];
-
-      # `brew install --cask`
-      casks = [
-        "aerospace"
-        # "visual-studio-code"
-      ];
-    };
+    # `brew install --cask`
+    casks = [ ];
+  };
 
 }
