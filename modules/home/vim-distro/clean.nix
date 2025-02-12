2a078a3ab583f2fc8a039a514500314c00650041
Antony Blakey <antony.blakey@gmail.com>
2025-02-12 17:45:10 +1100

    cleanup, re-add devenv

--- modules/home/vim-distro/clean.nix
+++
@@ -1,9 +0,0 @@
-{ writeShellApplication }:
-writeShellApplication {
-  name = "vim-distro-format";
-  text = ''
-    test $# -eq 0 && echo "Missing application name" && exit 1 
-    set -x
-    rm -rf "''${XDG_CONFIG_HOME:?}/$1" "''${XDG_CACHE_HOME:?}/$1" "''${XDG_DATA_HOME:?}/$1" "''${XDG_STATE_HOME:?}/$1"
-  '';
-}
--- modules/home/wezterm/default.nix
+++
@@ -1,9 +0,0 @@
-{ pkgs, ... }:
-{
-  home = {
-    packages = [ pkgs.wezterm ];
-    file = {
-      ".wezterm.lua".source = ./wezterm.lua;
-    };
-  };
-}
